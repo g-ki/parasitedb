a0d531f3abdfa11625ea64740d76a591ce26609a
Iliya Zhechev <i4ko95@gmail.com>
2018-04-21 22:26:06 +0300

    Make indentation - 2 spaces

--- src/hosts/hastebin.js
+++ src/hosts/hastebin.js
@@ -1,25 +1,25 @@
 const axios = require('axios');
 
 module.exports = class Hastebin {
-    constructor(hasteServer = 'https://hastebin.com') {
-        this.hasteServer = hasteServer;
-    }
+  constructor(hasteServer = 'https://hastebin.com') {
+    this.hasteServer = hasteServer;
+  }
 
-    get(url) {
-        return axios.get(url).then(res => res.data);
-    }
+  get(url) {
+    return axios.get(url).then(res => res.data);
+  }
 
-    has(url) {
-        return axios.get(url).then(() => true, () => false);
-    }
+  has(url) {
+    return axios.get(url).then(() => true, () => false);
+  }
 
-    set(value) {
-        return axios.post(`${this.hasteServer}/documents`, value)
-            .then(res => `${this.hasteServer}/raw/${res.data.key}`);
-    }
+  set(value) {
+    return axios.post(`${this.hasteServer}/documents`, value)
+      .then(res => `${this.hasteServer}/raw/${res.data.key}`);
+  }
 
-    unset() {
-        return Promise.resolve();
-    }
+  unset() {
+    return Promise.resolve();
+  }
 };
 
