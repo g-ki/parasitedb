11df131f84b37402d62605a12a59c49fce9d3c7a
Iliya Zhechev <i4ko95@gmail.com>
2018-04-21 22:32:32 +0300

    Gitignore example db

--- examples/simple.js
+++ examples/simple.js
@@ -9,9 +9,10 @@ const Host = require('../src/hosts/host');
 const Hastebin = require('../src/hosts/hastebin');
 
 const SECRET_MESSAGE = 'qwerty12346';
+const rootLocation = './data.pdb';
 
 const hosts = {
-  nullHost,
+  // nullHost,
   izDemon: new Host('http://172.27.29.155:3000'),
   hastebin: new Hastebin()
 };
@@ -19,11 +20,11 @@ const hosts = {
 (async () => {
   const db = await encryptor(
     SECRET_MESSAGE,
-    pdb('./users.pdb', hosts)
+    pdb(rootLocation, hosts)
   );
 
   await db.set('bob', { name: 'Bob', age: 21 });
-  await db.set('file', await fromFile('./users.pdb'));
+  await db.set('file', await fromFile(rootLocation));
 
   const user = await db.get('bob');
   const file = await db.get('file');
